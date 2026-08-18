c9fd2b2cce464ff3c74df9969a07b3de2307d1f1
subin-uiux <apwnrhf3@gmail.com>
2026-08-18 17:17:04 +0900

    test 페이지 완

--- equil/assets/js/common.js
+++ equil/assets/js/common.js
@@ -100,6 +100,11 @@
 
   const initHeading3tierFadeUp = () => {
     document.querySelectorAll('.heading-3tier').forEach((heading) => {
+      /* 질문 헤딩은 좁은 폭에서 단어 단위 줄바꿈을 유지해야 해서 글자 분리를 하지 않음 */
+      if (heading.classList.contains('sleep-fit-test__question-heading')) return;
+      if (heading.classList.contains('sleep-fit-result__heading')) return;
+      if (heading.classList.contains('sleep-fit-structure__heading')) return;
+
       const targets = [
         heading.querySelector('.heading-3tier__sub-title'),
         heading.querySelector('.heading-3tier__title'),
